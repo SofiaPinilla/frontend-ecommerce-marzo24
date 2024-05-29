3e2081de31652cbd27b5d16c94b61b6ae3bfa8bd
SofiaPinilla <contact@sofiapinilla.com>
2024-05-29 10:40:58 +0200

    flujo compra implementado

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,25 @@ import { UserProvider } from "./context/UserContext/UserState";
 import Profile from "./components/Profile/Profile";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
+import { ProductProvider } from "./context/ProductContext/ProductState";
+import Products from "./components/Products/Products";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <UserProvider>
-          <Header/>
-          <Routes>
-            <Route path="/" element={ <Home />}/>
-            <Route path="/login" element={ <Login />}/>
-            <Route path="/profile" element={ <Profile />}/>
-          </Routes>
+          <ProductProvider>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </ProductProvider>
         </UserProvider>
       </BrowserRouter>
     </>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../../context/UserContext/UserState";
+import {ShoppingCartOutlined} from "@ant-design/icons"
+import { Badge } from 'antd';
+import { ProductContext } from "../../context/ProductContext/ProductState";
 
 const Header = () => {
   const { token, logout } = useContext(UserContext);
+  const {cart} = useContext(ProductContext)
   const navigate = useNavigate();
+
+  useEffect(()=>{
+    localStorage.setItem("cart",JSON.stringify(cart))
+  },[cart])
 
   return (
     <div>
-      Header
-      <Link to="/">Home</Link>
+      Logo
+      <Link to="/"> Home /</Link>
+      <Link to="/products"> Products /</Link>
       {token ? (
         <>
-          <Link to="/profile">Profile</Link>
+          <Link to="/profile"> Profile /</Link>
+          <Link to="/cart"> Cart <Badge count={cart.length} size="small"><ShoppingCartOutlined /></Badge> /</Link>
           <button
             onClick={() => {
               logout();
               navigate("/login");
             }}
           >
-            Logout
+            Logout /
           </button>
         </>
       ) : (
-        <Link to="/login">Login</Link>
+        <Link to="/login"> Login </Link>
       )}
     </div>
   );
